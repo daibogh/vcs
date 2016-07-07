2609afd98e97ec0f404e59843bc66ce9e1ca8904
xLoSyAsHx <losyashiks200797>
2016-07-07 14:23:27 +0300

    interface.py и help().txt
    Сделал interface с использованием словаря и добавил help().txt

--- practice_work/interface.py
+++ practice_work/interface.py
@@ -1,98 +1,45 @@
-'''
-Команды для интерфейса:
-	help
-	set_prj
-	set_file
-	set_ver   выбрать версию 
-	show_prjs
-
-	commit
-	get_status
-
-	exit
-
-	push
-	add_prj
-	можно добавить команду клонирования в выбранный каталог
-'''
 def help():
-	''Можно сделать вывод из файла''
-	print('\t\tСписок команд:\n')
-	print('\thelp - Вывести список команд')
-	print('\tshow_prjs - Вывести список всех проектов')
-	print('\tset_prj - Выбрать проект')
-	print('\tadd_prj - Добавить существующий проект')
-	print('\tset_ver - Выбрать версию проекта (по умолчанию стоит последняя версия)')
-	print('\tset_file - Выбрать файл проекта')
-	print()
-	print('\tadd - Выбрать файлы для добавления в финальную версию проекта (проиндексировать файл)')
-	print('\tcommit - Добавить выбранные файлы в финальную версию проекта')
-	print('\tpush - Добавить последние изменения на сервер')
-	print('\tdel_in_index - Удалить файл из индекса (отслеживаемых файлов)')
-	print('\tdel_file - Удалить файл из системы')
-	#print('\t\push - Добавить ваши изменения в финальную версию проекта')
-	print('\tget_status - Вывести текущее состояние всех файлов')
-	print('\texit')
+	f = open('help().txt','r')
+	for line in f:
+		print(line)
+def show_prjs():
+def set_prj():
+def add_prj():
+def set_ver():
+def set_file():
+def add():
+def commit():
+def del_in_index():
+def del_file():
+def get_status():
+def exit():
 
-def execute_query(command):
-	if command == 'help':
-		help()
-	elif command == 'show_prjs':
-<<<<<<< HEAD
+dict_command = {
+	'help':help,
+	'show_prjs':show_prjs,
+	'set_prj':set_prj,
+	'add_prj':add_prj,
+	'set_ver':set_ver,
+	'set_file':set_file,
+	'add':add,
+	'commit':commit,
+	'del_in_index':del_in_index,
+	'del_file':del_file,
+	'get_status':get_status,
+	'exit':exit
+}
 
-	elif command == 'set_prj':
-
-	elif command == 'add_prj':
-
-	elif command == 'set_ver':
-
-	elif command == 'set_file':
-
-	elif command == 'add':
-
-	elif command == 'commit':
-
-	elif command == 'del_in_index':
-
-	elif command == 'del_file':
-
-	elif command == 'get_status':
-
-	elif command == 'exit':
-		
-=======
-		/
-	elif command == 'set_prj':
-		/
-	elif command == 'add_prj':
-		/
-	elif command == 'set_ver':
-		/
-	elif command == 'set_file':
-		/
-	elif command == 'add':
-		/
-	elif command == 'commit':
-		/
-	elif command == 'del_in_index':
-		/
-	elif command == 'del_file':
-		/
-	elif command == 'get_status':
-		/
-	elif command == 'exit':
-		/
->>>>>>> parent of 14a90f9... Поправил синтаксис в interface
-	else:
-		print('Такой команды нет. Пожалуйста, повторите ввод.')
-		help()
-
-import commands
+#import commands
 def interface(user):
 	print("выберите команду(чтобы узнать список команд, наберите help)")
 	print('>>', end=' ')
-	while (command = import()) != 'exit':
-		execute_query(command)
+	while True:
+		command = input()
+		if dict_command.get(command) != None:
+			dict_command[command]()
+		else:
+			print('Такой команды нет. Пожалуйста, повторите ввод.')
+			help()
+		if command == 'exit':
+			break
 		print('>>', end=' ')
-
-

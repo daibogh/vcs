311a0a806e51333c80e6478672245c8b9b02cc9f
Daibogh <DaiBogh@gmail.com>
2016-07-07 15:24:02 +0300

    user commands
    добавлены прототипы команд пользователя

--- practice_work/user_commands.py
+++ practice_work/user_commands.py
@@ -1,5 +1,16 @@
+import variables as var
 def check_updates(username):
-	
+	for
 def commit(username):
-
+	stack = get_stack()
+	stack.append(updates)
+	f = open("version_control.txt","wb")
+	pickle.dump(stack,f)
+	f.close()
 def push(username):
+	check = check_updates()
+	if check:
+		f = open(var.global_destination+project_name+"/global_control_version.txt","rb")
+		global_stack = pickle.load(f)
+		f.close()
+		f = open()

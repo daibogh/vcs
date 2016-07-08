465168e67318d4f8e4fd7d10a677b67b48f59d14
Kraynov Dmitry <dima132krainov@yandex.ru>
2016-07-08 15:18:20 +0300

    stack
    Добавление функции add_commit для занесения изменений в stack

--- practice_work/stack_commands.py
+++ practice_work/stack_commands.py
@@ -1,7 +1,7 @@
 import pickle
 import variables as var
 from datetime import datetime
-
+import changes_in_global as chingl
 
 def make_stack(username,project_name):
 	stack = []
@@ -9,22 +9,32 @@ def make_stack(username,project_name):
 	element["user"] = username
 	element["date-time"] = datetime.now()
 	path_to_stack = "/"+"/".join([project_name,"stack.txt"])
-	element["changes"] = {path_to_stack:"added"}
+	element["changes"] = {path_to_stack:"+"}
 	stack.append(element)
 	return stack
-
-
-
-
-
-
 
 def load_stack():
 	f = open(var.global_destination+"version_control.txt","rb")
 	global_version_control = pickle.load(f)
 	f.close()
 	return global_version_control
+
 def dump_stack(stack):
 	f = open(var.global_destination+"version_control.txt","wb")
+	pickle.dump(stack,f)
+	f.close()
+
+def add_commit(username,project_name):
+	element = {}
+	element["user"] = username
+	element["date-time"] = datetime.now()
+	element["changes"]=chingl.global_changes(username,project_name)
+	if element["changes"]=={}:
+		print("Не было внесено никаких изменений")
+		return
+	path_to_stack = var.users_destination+"stack.txt"
+	f = open(path_to_stack,"rb")
+	stack = pickle.load(f)
+	stack.append(element)
 	pickle.dump(stack,f)
 	f.close()

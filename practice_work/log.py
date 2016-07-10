7aaafa88e9820be0dc8efae3fa61da4bb18c7b24
Daibogh <DaiBogh@gmail.com>
2016-07-10 05:35:37 +0300

    log
    начата работа с функцией log

---
+++ practice_work/log.py
@@ -0,0 +1,32 @@
+import variables as var
+import stack_commands as sc
+def pre_log(project_name):
+	if input().split()[0] == "log":
+		log(" ".join(input().split()[1:]))
+
+def log(project_name, argument):
+	global_stack  = sc.load_g(project_name)
+	if argument == "--name-only":
+		for commit in global_stack:
+			print("#####################################################################")
+			print(commit["date-time"])
+			for element in commit["changes"].keys():
+				print(commit["changes"][element][0],element,sep = " --- ")
+			print("#####################################################################")
+	elif argument == "--reverse":
+		for commit in reversed(global_stack):
+			print("#####################################################################")
+			print(commit["date-time"])
+			for element in commit["changes"].keys():
+				print(commit["changes"][element][0],element,sep = " --- ")
+			print("#####################################################################")
+	elif argument.split()[0] == "--after":
+		pass
+		
+
+def main():
+	log("n","--name-only")
+
+if "__name__" == "__main__":
+	main()
+
